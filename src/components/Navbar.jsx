aab3ffd1ebc1827b975d534c088b9fb5ff12451e
shivansh sharma <105637680+Shivansh-Sharma-134@users.noreply.github.com>
2025-05-20 16:46:11 +0530

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 
 function Navbar() {
   return (
-     <nav className="bg-gray-800 text-white px-6 py-4 flex justify-between items-center">
+     <nav className="bg-gray-800 text-white pl-5 h-20 flex justify-between items-center ">
       <a href='/'>
       <div className="text-2xl font-bold ">Bloggit</div>
       </a>
-      <ul className="flex gap-6">
+      <ul className="flex h-full ">
         
-        <li><a href="/" className="hover:text-gray-300">Home</a></li>
-        <li><a href="/about" className="hover:text-gray-300">About</a></li>
-        <li><a href="/log-in" className="hover:text-gray-300">Sign Up</a></li>
-        <li><a href="/sign-up" className="hover:text-gray-300">Log In</a></li>
+        <li><a href="/" className="block px-4 py-6 h-full hover:text-black hover:bg-white text-center">Home</a></li>
+        <li><a href="/about" className="block   px-4 py-6 h-full hover:text-black hover:bg-white">About</a></li>
+        <li><a href="/log-in" className="block  px-4 py-6 h-full hover:text-black hover:bg-white">Log In</a></li>
+        <li><a href="/sign-up" className="block px-4 py-6 h-full hover:text-black hover:bg-white">Log In</a></li>
       </ul>
     </nav>
+
   )
 }
 
